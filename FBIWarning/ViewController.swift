1431c69b93ec253963c2552f73f02fec49fac4c0
Sun <vu.xuan.cuong@framgia.com>
2019-03-04 19:47:06 +0700

    Initial Commit

---
+++ FBIWarning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FBIWarning
+//
+//  Created by mac on 3/4/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
